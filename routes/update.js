bbd751c6a32d7ea74e9b4339af9713e28fbe1564
Danny <dannyvdbiezen@outlook.com>
2015-03-20 13:51:53 +0100

    speciaal voor joost

--- routes/update.js
+++ routes/update.js
@@ -1,3 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'updates hier', body: req.body });
 });
