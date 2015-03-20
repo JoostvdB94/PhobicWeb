405f4f4c87a1e6fb7c610790abfe5686edbb6fb8
Danny <dannyvdbiezen@outlook.com>
2015-03-20 19:48:38 +0100

    api aangemaakt

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,6 @@
-/**
- * Created by joost on 20-3-15.
- */
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     res.json([{
         "duration" : 1000,
