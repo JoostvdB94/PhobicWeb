8084e8d8a384b86501daedf7bab789e388b2b56a
Danny <dannyvdbiezen@outlook.com>
2015-03-21 03:15:49 +0100

    api uitgebreidt

--- routes/api.js
+++ routes/api.js
@@ -4,21 +4,18 @@ var app = express();
 
 var mongoose = require('mongoose');
 var Phobia = mongoose.model('phobia');
+var Relaxationmethod = mongoose.model('relaxationmethod');
 
 ////////////////////////////////////phobia/////////////////////////////////////////////////////
 router.get("/phobia", function (req, res, next) {
       Phobia.find( function ( err, phobia, count ){
-    	res.json(phobia
-	     		  );
-  });
+    	res.json(phobia);
+  }).populate('relaxationmethods');
 });
 
 router.get("/phobia/:id", function (req, res, next) {
     Phobia.findById( req.params.id, function ( err, phobia ){
-    	res.json({ _id: phobia._id,
-	     			name: phobia.name,
-	     			errors: err
-	     		  });
+    	res.json(phobia);
   	});
 });
 
@@ -26,10 +23,7 @@ router.post("/phobia", function (req, res, next) {
 	new Phobia({
 	    name    : req.body.name
 	  }).save( function( err, phobia, count ){
-	     res.json({ _id: phobia._id,
-	     			name: phobia.name,
-	     			errors: err
-	     		  });
+	     res.json(phobia);
 	  });
 });
 
@@ -37,48 +31,73 @@ router.put("/phobia/:id", function (req, res, next) {
     Phobia.findById( req.params.id, function ( err, phobia ){
 	    phobia.name    = req.body.name;
 	    phobia.save( function ( err, phobia, count ){
-	        res.json({ _id: phobia._id,
-	     			name: phobia.name,
-	     			errors: err
-	     		  });
+	        res.json(phobia);
 	    });
 	});
 });
 
 router.delete("/phobia/:id", function (req, res, next) {
       Phobia.findById( req.params.id, function ( err, phobia ){
-    phobia.remove( function ( err, phobia ){
-      res.json({ _id: phobia._id,
-	     			name: phobia.name
-	     		  });
-    });
+	    phobia.remove( function ( err, phobia ){
+	      res.json(phobia);
+	    });
   });
 });
 //////////////////////////////////////endphobia/////////////////////////////////////////////////
 
 //////////////////////////////////////relaxationmethod//////////////////////////////////////////
 router.get("/phobia/:id/relaxationmethod", function (req, res, next) {
-      res.render('index', { title: 'overview relaxation', body: req.body });
+      Relaxationmethod.find( function ( err, relaxationmethod, count ){
+    	res.json(relaxationmethod);
+  }).populate('relaxationmethod');
 });
 
 router.post("/phobia/:id/relaxationmethod", function (req, res, next) {
-      res.render('index', { title: req.params.name, body: "post"});
+      var relax = new Relaxationmethod({
+	    name    		: req.body.name,
+	    activities 		: req.body.activities,
+	    phobia 			: req.params.id,
+	    effectiveness 	: req.body.effectiveness,
+	    times_used 		: req.body.times_used
+	  });
+
+	  relax.save( function( err, phobia, count ){
+	  	console.log(err);
+	     res.json(phobia);
+	  });
+
+	  Phobia.findById( req.params.id, function ( err, phobia ){
+	    phobia.relaxationmethods.push(relax);
+	    phobia.save( function ( err, phobia, count ){
+	    });
+	});
 });
 
-router.post("/phobia/:id/relaxationmethod/:id", function (req, res, next) {
-      res.render('index', { title: req.params.name + " " + req.params.id, body: req.params.id });
+router.get("/phobia/:id/relaxationmethod/:relaxid", function (req, res, next) {
+      Relaxationmethod.findById( req.params.relaxid, function ( err, relaxationmethod ){
+    	res.json(relaxationmethod);
+  	});
 });
 
-router.get("/phobia/:id/relaxationmethod/:id", function (req, res, next) {
-      res.render('index', { title: req.params.name + " " + req.params.id, body: req.params.id });
+router.put("/phobia/:id/relaxationmethod/:relaxid", function (req, res, next) {
+      Relaxationmethod.findById( req.params.relaxid, function ( err, relaxationmethod ){
+	    relaxationmethod.name    		= req.body.name;
+	    relaxationmethod.activities 		= req.body.activities;
+	    relaxationmethod.phobia 			= req.params.id;
+	    relaxationmethod.effectiveness 	= req.body.effectiveness;
+	    relaxationmethod.times_used 		= req.body.times_used;
+	    relaxationmethod.save( function ( err, relaxationmethod, count ){
+	        res.json(relaxationmethod);
+	    });
+	});
 });
 
-router.put("/phobia/:id/relaxationmethod/:id", function (req, res, next) {
-      res.render('index', { title: req.params.name + " " + req.params.id, body: req.params.id });
-});
-
-router.delete("/phobia/:id/relaxationmethod/:id", function (req, res, next) {
-      res.render('index', { title: req.params.name + " " + req.params.id, body: req.params.id });
+router.delete("/phobia/:id/relaxationmethod/:relaxid", function (req, res, next) {
+      Relaxationmethod.findById( req.params.relaxid, function ( err, relaxationmethod ){
+	    relaxationmethod.remove( function ( err, relaxationmethod ){
+	      res.json(relaxationmethod);
+	    });
+  });
 });
 //////////////////////////////////////endrelaxationmethod///////////////////////////////////////
 module.exports = router;
